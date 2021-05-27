1b210062e9e90a332008dda7a5c96777c7bad5fb
Uitumen <t.uitumen@gmail.com>
2021-05-27 19:20:11 +0800

    561

---
+++ Problems/src/LeetCode/ArrayPartitionI.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 4, 3, 2 };
+		Arrays.sort(arr);
+		int res = arrayPairSum(arr);
+		System.out.println(res);
+	}
+
+	public static int arrayPairSum(int[] nums) {
+		int res = 0;
+		for (int i = 0; i < nums.length - 1; i += 2) {
+			res += Math.min(nums[i], nums[i + 1]);
+		}
+		return res;
+	}
+}
---
+++ Problems/src/LeetCode/ww.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+public class ww {
+	public static void main(String args[]) {
+		int i = 5;
+		int b = i++;
+		System.out.println(b);
+		System.out.println(i);
+	}
+}
